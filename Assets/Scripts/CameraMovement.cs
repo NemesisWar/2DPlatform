849bcfe107a326ba029dadcae3ebff2a66c4328c
NemesisWar <66144477+NemesisWar@users.noreply.github.com>
2020-06-21 00:42:56 +0300

    comment
    comment

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,14 +7,12 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private Transform _player;
     [SerializeField] private Vector3 _lookCameraPosition;
     [SerializeField] private Transform _camera;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _lookCameraPosition = _camera.position - new Vector3(_player.position.x, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         _camera.position=_lookCameraPosition + new Vector3(_player.position.x, 0, 0);
     }
