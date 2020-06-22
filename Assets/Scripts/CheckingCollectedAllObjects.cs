72d58e7b28e74ae34624b2b50837e1c19e6b66fd
NemesisWar <66144477+NemesisWar@users.noreply.github.com>
2020-06-23 00:52:46 +0300

    comment
    rename SpawnPointsController to CheckingCollectedAllObjects

---
+++ Assets/Scripts/CheckingCollectedAllObjects.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckingCollectedAllObjects : MonoBehaviour
+{
+    [SerializeField] private JaguarTrigger[] _points;
+
+    private void OnEnable()
+    {
+        _points = GetComponentsInChildren<JaguarTrigger>();
+        foreach(var point in _points)
+        {
+            point.Reached += AllObjectsCollected;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var point in _points)
+        {
+            point.Reached -= AllObjectsCollected;
+        }
+    }
+
+    public void AllObjectsCollected()
+    {
+        foreach (var point in _points)
+        {
+            if (point.IsReached == false)
+                return;
+
+        }
+        Debug.Log("Congradulation You Are Alcoholic!");
+    }
+
+
+}
